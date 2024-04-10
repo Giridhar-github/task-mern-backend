f88c6ca96dad4c86fab25d25927c2d7bdacbb208
Giridhar-github <57524550+Giridhar-github@users.noreply.github.com>
2024-04-10 19:59:42 +0530

    cors added

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const { errorHandler } = require("./middleware/errorMiddleware");
 const dotenv = require("dotenv").config();
 const port = process.env.PORT || 5000;
 const connectDB = require("./connect/database");
-
+const Cors = require("cors");
 connectDB();
 const app = express();
-
+app.use(Cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
